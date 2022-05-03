bd2a59f8d920743a1c99cf47961c67b022e5bded
sukhumarn.a <sukhumarn.a@kbtg.tech>
2022-05-03 10:59:06 +0700

    add functions of biclass_env class

--- environment/biclass_env.py
+++ environment/biclass_env.py
@@ -1,6 +1,7 @@
 import numpy as np
 from gym import spaces
 from gym.core import Env
+from sklearn.metrics import classification_report, roc_auc_score
 
 
 class BiClassEnvironment(Env):
@@ -23,14 +24,52 @@ class BiClassEnvironment(Env):
         self.time_step = 0
 
         self.actions = []
+        self.seed()
+
+    def seed(self, seed=0):
+        np.random.seed(seed)
 
     def step(self, action):
-        print("step function")
+        terminal = False
+        self.actions.append(action)
+        # positive class
+        if self.data_y[self.index[self.time_step]] == 1:
+            # if correct prediction set reward to 1, otherwise -1
+            if self.data_y[self.index[self.time_step]] == action:
+                reward = 1
+            else:
+                reward = -1
+                terminal = True
+        # negative class
+        else:
+            # if correct prediction set reward to reward ratio, otherwise minus reward ratio
+            if self.data_y[self.index[self.time_step]] == action:
+                reward = self.reward_ratio
+            else:
+                reward = -self.reward_ratio
+
+        self.time_step = self.time_step + 1
+
+        # finish all samples, set terminal to true (finish the episode)
+        next_state = None
+        if self.time_step == self.episode_len:
+            terminal = True
+        else:
+            next_state = self.data_x[self.index[self.time_step]]
+
+        return next_state, reward, terminal
 
     def render(self, mode="human"):
-        print("render function")
+        print('at time step {}/{}'.format(self.time_step, self.episode_len))
+        print(classification_report(self.data_y[self.index], self.actions))
+        rocauc = roc_auc_score(self.data_y[self.index], self.actions)
+        print('rocauc score is', rocauc)
+        return rocauc
 
     def reset(self):
-        print("reset function")
+        np.random.shuffle(self.index)
+        self.time_step = 0
+        self.actions = []
+        return self.data_x[self.index[self.time_step]]
 
 
--- test/test_biclass_env.py
+++ test/test_biclass_env.py
@@ -7,6 +7,12 @@ from environment.biclass_env import BiClassEnvironment
 
 class TestBiClassEnvironment(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.data_x = np.array([[0.1, 1.0], [0.2, 2.0], [0.3, 3.0], [0.4, 4.0], [0.5, 5.0]])
+        cls.data_y = np.array([1, 0, 0, 1, 0])
+        cls.pos_neg_ratio = 2.0/3.0
+
     def test_biclass_environment(self):
         data = {'data1': [0.5, 0.7, 0.3, 0.6, 0.1], 'data2': [0.32, 0.56, 0.27, 0.74, 0.25]}
         df = pd.DataFrame(data, columns=['data1', 'data2'])
@@ -30,6 +36,70 @@ class TestBiClassEnvironment(unittest.TestCase):
         data_y = np.array([1, 0, 0, 0, 2])
         self.assertRaises(ValueError, BiClassEnvironment,
                           data_x=data_x, data_y=data_y, pos_neg_ratio=pos_neg_ratio)
+
+    def test_step(self):
+        env = BiClassEnvironment(self.data_x, self.data_y, self.pos_neg_ratio)
+        # set index in order to test in sequence
+        env.index = np.arange(5)
+
+        # case correct positive class
+        next_state, reward, terminal = env.step(action=1)
+        self.assertEqual(reward, 1)
+        self.assertTrue(np.array_equal(np.array([0.2, 2.0]), next_state))
+        self.assertFalse(terminal)
+
+        # case correct negative class
+        next_state, reward, terminal = env.step(action=0)
+        self.assertAlmostEqual(reward, self.pos_neg_ratio, places=2)
+        self.assertFalse(terminal)
+
+        # case incorrect negative class (action=1, actual=0)
+        next_state, reward, terminal = env.step(action=1)
+        self.assertAlmostEqual(reward, -self.pos_neg_ratio, places=2)
+        self.assertFalse(terminal)
+
+        # case incorrect positive class (action=0, actual=1)
+        next_state, reward, terminal = env.step(action=0)
+        self.assertEqual(reward, -1)
+        self.assertTrue(terminal)
+
+        # correct prediction, last step in the episode
+        next_state, reward, terminal = env.step(action=0)
+        self.assertAlmostEqual(reward, self.pos_neg_ratio, places=2)
+        self.assertTrue(terminal)
+        self.assertIsNone(next_state)
+
+    def test_reset(self):
+        env = BiClassEnvironment(self.data_x, self.data_y, self.pos_neg_ratio)
+
+        env.seed(seed=1)
+        env.step(action=1)
+        env.step(action=1)
+        self.assertTrue(np.array_equal(np.array([1, 1]), env.actions))
+        self.assertEqual(env.time_step, 2)
+
+        state = env.reset()
+        self.assertTrue(np.array_equal(np.array([]), env.actions))
+        self.assertEqual(env.time_step, 0)
+        self.assertTrue(np.array_equal(self.data_x[env.index[0]], state))
+
+    def test_render(self):
+        env = BiClassEnvironment(self.data_x, self.data_y, self.pos_neg_ratio)
+        env.index = np.arange(5)
+        # actual:        [1, 0, 0, 1, 0]
+        # prediction:    [1, 0, 0, 0, 1]
+        actions = [1, 0, 0, 0, 1]
+        for a in actions:
+            env.step(action=a)
+        env.render()
+
+        env = BiClassEnvironment(self.data_x, self.data_y, self.pos_neg_ratio)
+        env.index = [1, 2, 3, 0, 4]
+        actions = [0, 0, 0, 1, 1]
+        for a in actions:
+            env.step(action=a)
+        rocauc = env.render()
+        self.assertAlmostEqual(rocauc, 0.583, places=3)
 
 
 if __name__ == '__main__':
