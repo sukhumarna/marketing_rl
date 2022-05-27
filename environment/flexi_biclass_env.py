cda3406ff09768a4372c469ce62704cd4eca9191
sukhumarn.a <sukhumarn.a@kbtg.tech>
2022-05-27 10:03:35 +0700

    add flexi_biclass_env

---
+++ environment/flexi_biclass_env.py
@@ -0,0 +1,92 @@
+import numpy as np
+from environment.env_mode import EnvMode
+from gym import spaces
+from gym.core import Env
+from sklearn.metrics import classification_report
+
+
+class FlexiBiClassEnvironment(Env):
+
+    def __init__(self, data_x, data_y, pos_neg_ratio, reward, mode=EnvMode.TRAIN, early_stop=None):
+        super(FlexiBiClassEnvironment, self).__init__()
+        if data_x.shape[0] != data_y.shape[0]:
+            raise ValueError("len of data_x and data_y is not equal")
+        self.data_x = data_x
+        self.data_y = data_y
+        self.reward_ratio = pos_neg_ratio
+        self.reward = reward
+        self.mode = mode
+        self.pos_miss_count = 0
+        self.num_classes = len(set(self.data_y))
+        if self.num_classes != 2:
+            raise ValueError("only 2 classes are allowed")
+        self.episode_len = self.data_x.shape[0]
+        self.index = np.arange(self.episode_len)
+        self.action_space = spaces.Discrete(self.num_classes)
+        self.time_step = 0
+        self.early_stop = early_stop
+
+        self.actions = []
+        self.seed()
+
+    def _get_index(self):
+        if self.time_step >= len(self.index):
+            return self.index[0]
+        return self.index[self.time_step]
+
+    def _positive(self) -> bool:
+        return self.data_y[self._get_index()] == 1
+
+    def _correct_prediction(self, action) -> bool:
+        return self.data_y[self._get_index()] == action
+
+    def seed(self, seed=0):
+        np.random.seed(seed)
+
+    def step(self, action):
+        info = {}
+        terminal = False
+        self.actions.append(action)
+        # positive class
+        if self._positive():
+            if self._correct_prediction(action):
+                reward = self.reward[self._get_index()]
+            else:
+                reward = -1
+                if self.mode == EnvMode.TRAIN and self.early_stop is not None:
+                    self.pos_miss_count += 1
+                    if self.pos_miss_count > self.early_stop:
+                        terminal = True
+        # negative class
+        else:
+            if self._correct_prediction(action):
+                reward = self.reward_ratio
+            else:
+                reward = -self.reward_ratio
+
+        self.time_step = self.time_step + 1
+
+        # finish all samples, set terminal to true (finish the episode)
+        if self.time_step == self.episode_len:
+            terminal = True
+            report = self.info()
+            label_1 = report['1']
+            info['precision'], info['recall'] = label_1['precision'], label_1['recall']
+            info['f1-score'] = label_1['f1-score']
+            next_state = self.data_x[self.index[0]]
+        else:
+            next_state = self.data_x[self._get_index()]
+
+        return next_state, reward, terminal, info
+
+    def reset(self):
+        pass
+
+    def render(self, mode="human"):
+        pass
+
+    def info(self):
+        report = classification_report(self.data_y[self.index[:self.time_step]],
+                                       self.actions,
+                                       output_dict=True)
+        return report
---
+++ test/test_flexi_biclass_env.py
@@ -0,0 +1,105 @@
+import numpy as np
+import unittest
+from environment.flexi_biclass_env import FlexiBiClassEnvironment, EnvMode
+
+
+class TestFlexiBiClassEnvironment(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.data_x = np.array([[0.1, 1.0], [0.2, 2.0], [0.3, 3.0], [0.4, 4.0], [0.5, 5.0]])
+        cls.data_y = np.array([1, 0, 0, 1, 0])
+        cls.reward = np.array([50, 0, 0, 100, 0])
+        cls.pos_neg_ratio = 2.0/3.0
+
+    def test_get_index(self):
+        env = FlexiBiClassEnvironment(data_x=self.data_x, data_y=self.data_y, pos_neg_ratio=self.pos_neg_ratio,
+                                      reward=self.reward, early_stop=None)
+        env.index = [4, 2, 1, 0, 3]
+        self.assertEqual(env._get_index(), 4)
+        env.step(action=0)
+        self.assertEqual(env._get_index(), 2)
+        env.step(action=0)
+        self.assertEqual(env._get_index(), 1)
+        env.step(action=0)
+        self.assertEqual(env._get_index(), 0)
+        env.step(action=1)
+        self.assertEqual(env._get_index(), 3)
+        env.step(action=1)
+        self.assertEqual(env._get_index(), 4)
+
+    def test_positive(self):
+        env = FlexiBiClassEnvironment(data_x=self.data_x, data_y=self.data_y, pos_neg_ratio=self.pos_neg_ratio,
+                                      reward=self.reward, early_stop=None)
+
+        # label: 1, 0, 0, 1, 0
+        self.assertTrue(env._positive())
+        env.step(action=0)
+        self.assertFalse(env._positive())
+        env.step(action=0)
+        self.assertFalse(env._positive())
+        env.step(action=0)
+        self.assertTrue(env._positive())
+        env.step(action=1)
+        self.assertFalse(env._positive())
+        env.step(action=0)
+        self.assertTrue(env._positive())
+
+    def test_correct_prediction(self):
+        env = FlexiBiClassEnvironment(data_x=self.data_x, data_y=self.data_y, pos_neg_ratio=self.pos_neg_ratio,
+                                      reward=self.reward, early_stop=None)
+        # label: 1, 0, 0, 1, 0
+        self.assertTrue(env._correct_prediction(action=1))
+        env.step(action=1)
+        self.assertFalse(env._correct_prediction(action=1))
+        env.step(action=0)
+        self.assertTrue(env._correct_prediction(action=0))
+        env.step(action=0)
+        self.assertFalse(env._correct_prediction(action=0))
+        env.step(action=1)
+        self.assertTrue(env._correct_prediction(action=0))
+        env.step(action=0)
+        self.assertFalse(env._correct_prediction(action=0))
+
+    def test_step(self):
+        env = FlexiBiClassEnvironment(data_x=self.data_x, data_y=self.data_y, pos_neg_ratio=self.pos_neg_ratio,
+                                      reward=self.reward, early_stop=0)
+        # label: 1, 0, 0, 1, 0
+        # reward: 50, 0, 0, 100, 0
+
+        # case correct positive class
+        next_state, reward, terminal, info = env.step(action=1)
+        self.assertEqual(reward, 50)
+        self.assertTrue(np.array_equal(np.array([0.2, 2.0]), next_state))
+        self.assertFalse(terminal)
+
+        # case correct negative class
+        next_state, reward, terminal, info = env.step(action=0)
+        self.assertAlmostEqual(reward, self.pos_neg_ratio, places=2)
+        self.assertFalse(terminal)
+
+        # case incorrect negative class (action=1, actual=0)
+        next_state, reward, terminal, info = env.step(action=1)
+        self.assertAlmostEqual(reward, -self.pos_neg_ratio, places=2)
+        self.assertFalse(terminal)
+
+        # case incorrect positive class (action=0, actual=1)
+        next_state, reward, terminal, info = env.step(action=0)
+        self.assertEqual(reward, -1)
+        self.assertTrue(terminal)
+
+        # correct prediction, last step in the episode
+        next_state, reward, terminal, info = env.step(action=0)
+        self.assertAlmostEqual(reward, self.pos_neg_ratio, places=2)
+        self.assertTrue(terminal)
+        self.assertTrue(np.array_equal(np.array([0.1, 1.0]), next_state))
+
+        env = FlexiBiClassEnvironment(data_x=self.data_x, data_y=self.data_y, pos_neg_ratio=self.pos_neg_ratio,
+                                      reward=self.reward, early_stop=None)
+        for i in range(4):
+            _, _, terminal, _ = env.step(action=0)
+            self.assertFalse(terminal)
+
+
+if __name__ == '__main__':
+    unittest.main()
