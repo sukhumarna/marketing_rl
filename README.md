57b05ca6f710a0dc9d78aaa8a85918fc44b4fdbd
sukhumarn.a <sukhumarn.a@kbtg.tech>
2022-07-01 13:43:20 +0700

    feature: add flexi tf environment

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # marketing-rl-environment
 MarRL is a framework to create an environment for developing reinforcement learning agent for marketing problem
+
+[![made-with-python](https://badges.aleen42.com/src/python.svg)](https://www.python.org/)
+[![tensorflow](https://badges.aleen42.com/src/tensorflow.svg)](https://www.tensorflow.org/)
+![alt unittest](coverage-badge.svg "Unittest Coverage")
--- test/test_flexi_biclass_tf_env.py
+++ test/test_flexi_biclass_tf_env.py
@@ -1,19 +1,21 @@
 import numpy as np
 import unittest
 from tf_agents.environments import utils
-from environment.biclass_tf_env import BiClassTFEnv
+from environment.flexi_biclass_tf_env import FlexiBiClassTFEnv
 
 
-class BiClassTFEnvTestCase(unittest.TestCase):
+class FlexiBiClassTFEnvTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
         cls.data_x = np.array([[0.1, 1.0], [0.2, 2.0], [0.3, 3.0], [0.4, 4.0], [0.5, 5.0]])
         cls.data_y = np.array([1, 0, 0, 1, 0])
+        cls.reward = np.array([1.5, 0, 0, 2, 0])
         cls.pos_neg_ratio = 2.0/3.0
 
-    def test_biclass_tf_env(self):
-        env = BiClassTFEnv(data_x=self.data_x, data_y=self.data_y, pos_neg_ratio=self.pos_neg_ratio)
+    def test_flexi_biclass_tf_en(self):
+        env = FlexiBiClassTFEnv(data_x=self.data_x, data_y=self.data_y,
+                                reward=self.reward, pos_neg_ratio=self.pos_neg_ratio)
         utils.validate_py_environment(env, episodes=5)
 
 
